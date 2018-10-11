8a2a89f1b5713a6ae42859057c3206920c900c03
jonesy15 <jonesy1567@outlook.com>
2018-10-11 16:50:05 +0100

    SDL Mixer library
    SDL Mixer Library Added

--- ShootEmUpEngine/ShootEmUpEngine/SoundManager.h
+++ ShootEmUpEngine/ShootEmUpEngine/SoundManager.h
@@ -1,8 +1,16 @@
 #pragma once
+
+#ifndef _SOUNDMANAGER_H
+
+#include <SDL.h>
+#include <SDL_mixer.h>
+#include <iostream>
+
 class SoundManager
 {
 public:
 	SoundManager();
 	~SoundManager();
 };
+#endif
 
